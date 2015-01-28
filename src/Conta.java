d1dd2165ae469566aeae41f9e8164a3a31f5f619
Lucas Jordani <lucas_jjjj@hotmail.com>
2015-01-28 15:42:52 -0200

    <Jordani, Patricia>

---
+++ src/Conta.java
@@ -0,0 +1,14 @@
+
+public class Conta {
+	
+	private int cem;
+	private int cinquenta;
+	private int vinte;
+	private int dez;
+		
+	public void sacar(double valor){
+		double aux;
+		
+		cem = (int) valor / 100;
+	}
+}
